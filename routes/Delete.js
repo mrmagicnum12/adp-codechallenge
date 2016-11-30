80ad68ac576909eaf8784dc01a77aa4e0060a84d
Anthony Buchanan <abuchanan@appsolutionsal.com>
2016-11-30 07:09:17 -0600

    added return to each rest handler

--- routes/Delete.js
+++ routes/Delete.js
@@ -1,10 +1,10 @@
 module.exports = function (app) {
   app.delete('/api/:component/:apiTitle', function (req, res) {
     try {
-      require("../backend/components/"+req.params.component+"/"+req.params.apiTitle)(req, res);
+      return require("../backend/components/"+req.params.component+"/"+req.params.apiTitle)(req, res);
     } catch (e) {
       res.status(500).send("Error in DELETE "+req.params.component+""+req.params.apiTitle+":"+e.message);
-      console.error(e);
+      return console.error(e);
     }
   });
 }
